0e7c2d7a565402bdfa6006b0c9462a9ffea5a840
Mik3Mon <mmonzonjr@gmail.com>
2020-05-19 12:56:52 -0500

    Se sube Swing2

---
+++ IntroSwing2/src/introswing2/IntroSwing2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introswing2;
+
+import ico.fes.poo.gui.Ventana;
+
+/**
+ *
+ * @author dmathews
+ */
+public class IntroSwing2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+    }
+    
+}
